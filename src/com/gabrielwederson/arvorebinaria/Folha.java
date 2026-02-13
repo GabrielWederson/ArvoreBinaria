047ddf23351df81202f203047785bd25668c0cad
Gabriel Wederson <gabrielwederson582@gmail.com>
2026-02-13 19:49:07 -0300

    criacao do metodo dos nos/folhas

---
+++ src/com/gabrielwederson/arvorebinaria/Folha.java
@@ -0,0 +1,12 @@
+package com.gabrielwederson.arvorebinaria;
+
+public class Folha {
+    public int valor;
+
+    public Folha(int valor){
+        this.valor = valor;
+    }
+    public int getValor(){
+        return this.valor;
+    }
+}
